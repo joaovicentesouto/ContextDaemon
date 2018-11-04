08304d420038499bd4d7b8ae61dec989f8aacccd
João Vicente Souto <joaovicentesouto@gmail.com>
2018-11-04 15:21:52 -0200

    Data cache - thinking of using SGD

--- src/context/CacheController.java
+++ src/context/CacheController.java
@@ -3,12 +3,14 @@ package context;
 import context.CacheController;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Calendar;
 import java.util.Collections;
 import java.util.Date;
 import java.util.Iterator;
 import java.util.LinkedList;
 //import java.time;
 import java.util.List;
+import java.util.TimeZone;
 
 import weka.core.Instance;
 import weka.core.Instances;
@@ -23,7 +25,7 @@ public class CacheController {
 	static long HUMIDITY = 2224179500L; //! VERIFICAR
 	
 	//! Max size of _instances = 1000
-	private List<Instance> _instantes;
+	private List<Instance> _instances;
 	private ArrayList<Attribute> attributes;
 	
 	private Instances _persistent_instances;
@@ -32,18 +34,19 @@ public class CacheController {
 	private Double avg_internal_temps = 0.0, avg_external_temps = 0.0, avg_internal_hums = 0.0, avg_external_hums = 0.0;
 	private int n_in_temp = 0, n_out_temp = 0, n_in_hum = 0, n_out_hum = 0;
 	
-	private Date current;
+	private Calendar _calendar = Calendar.getInstance(TimeZone.getTimeZone("UTC")); 
 
-	public CacheController() {
+	public CacheController() throws Exception {
 		super();
 		
-		_instantes = Collections.synchronizedList(new LinkedList<Instance>());
+		_instances = Collections.synchronizedList(new LinkedList<Instance>());
+		_calendar.setTimeInMillis(System.currentTimeMillis());
 		
 		reload_backup();
-		feature_selection();
+//		feature_selection();
 	}
 	
-	private void reload_backup() {
+	private void reload_backup() throws Exception {
 		
 		//! Carrega instancias persistentes
 		DataSource source = new DataSource("cache.arff");
@@ -69,78 +72,96 @@ public class CacheController {
 //	private void feature_selection() {
 //		attributes = new ArrayList<Attribute>(
 //			Arrays.asList(
-//				new Attribute("internal_temperature"),
-//				new Attribute("external_temperature"),
-//				new Attribute("internal_humidity"),
-//				new Attribute("external_humidity"),
-//				new Attribute("second"),
-//				new Attribute("minute"),
-//				new Attribute("hour"),
-//				new Attribute("day"),
-//				new Attribute("week_day"),
-//				new Attribute("month"),
-//				new Attribute("ideal_temperature")
+//				new Attribute("internal_temperature"),	fica
+//				new Attribute("external_temperature"),	fica
+//				new Attribute("internal_humidity"),		fica
+//				new Attribute("external_humidity"),		fica
+//				new Attribute("second"),				sai
+//				new Attribute("minute"),				fica
+//				new Attribute("hour"),					fica
+//				new Attribute("day"),					sai
+//				new Attribute("week_day"),				fica
+//				new Attribute("month"),					sai
+//				new Attribute("ideal_temperature")		fica
 //			)
 //		);
 //		
 //		_persistent_instances = new Instances("weather", attributes, 11);
 //	}
 
-	public synchronized void updateData(SmartData data) {
-		System.out.println("Data: " + data.toString());
-		
-		//attribute numeric
-	
-		Double aux;
-	
-		
-		if(data.getX() == 298 && data.getUnit() == TEMPERATURE) {
-			aux = internal_temps.get(internal_temps.size() - 1);
-			if(aux - data.getValue() < 0.1 || data.getValue() - aux < 0.1)
-				internal_temps.add(data.getValue());
-		} else if(data.getX() == 302 && data.getUnit() == TEMPERATURE) {
-			aux = external_temps.get(external_temps.size() - 1);
-			if(aux - data.getValue() < 0.1 || data.getValue() - aux < 0.1)
-				external_temps.add(data.getValue());
-		} else if(data.getX() == 298 && data.getUnit() == HUMIDITY) {
-			aux = internal_hums.get(internal_hums.size() - 1);
-			if(aux - data.getValue() < 0.1 || data.getValue() - aux < 0.1)
-				internal_hums.add(data.getValue());
-		} else if(data.getX() == 302 && data.getUnit() == HUMIDITY) {
-			aux = external_hums.get(external_hums.size() - 1);
-			if(aux - data.getValue() < 0.1 || data.getValue() - aux < 0.1)
-				external_hums.add(data.getValue());
-		}
-		
-		Double temp_in = internal_temps.get(index);
-		Double temp_out = external_temps.get(index);
-		Double hum_in = internal_hums.get(index);
-		Double hum_out = external_hums.get(index);
-		        
-		Date tstamp = timestamps.get(index);
-		
-		// transforming tstamp 
-		
-		Instance instance = new DenseInstance(12);
-		
-		instance.setValue(0, temp_in);
-		instance.setValue(1, temp_out);
-		instance.setValue(2, hum_in);
-		instance.setValue(3, hum_out);
-		
-		// add attributes
-		
-		instances.add(instance);
-		
-		index++;
-		
-		if(internal_temps.size() > 1000 || external_temps.size() > 1000 || internal_hums.size() > 1000 || external_hums.size() > 1000) {
+	public synchronized void updateData(SmartData data) throws Exception
+	{	
+		//! Se já se passaram 30 segundos => constrói uma instância
+		if (_calendar.getTimeInMillis()/1000l - data.getT() > 30) {
 			
+			Instance instance = new DenseInstance(8); //! _persistent_instances.size()
+			instance.setValue(0, avg_internal_temps);
+			instance.setValue(1, avg_external_temps);
+			instance.setValue(2, avg_internal_hums);
+			instance.setValue(3, avg_external_hums);
+			instance.setValue(4, _calendar.get(Calendar.MINUTE));
+			instance.setValue(5, _calendar.get(Calendar.HOUR_OF_DAY));
+			instance.setValue(6, _calendar.get(Calendar.DAY_OF_WEEK));
+			instance.setValue(7, 22); //! Comando do usuário
+			
+			_instances.add(instance);
+			
+			
+			//! Zera nova instância
+			_calendar.setTimeInMillis((data.getT() + 1) * 1000L);
+			avg_internal_temps = 0.0;
+			avg_external_temps = 0.0;
+			avg_internal_hums = 0.0;
+			avg_external_hums = 0.0;
+			n_in_temp = 0;
+			n_out_temp = 0;
+			n_in_hum = 0;
+			n_out_hum = 0;
 		}
+		
+		
+		switch (data.getX())
+		{
+		//! Interno
+		case 298:
+			if (data.getUnit() == TEMPERATURE) {
+				n_in_temp++;
+				avg_internal_temps = avg_internal_temps + (data.getValue() - avg_internal_temps) / n_in_temp;
+			} else { //! HUMIDITY
+				n_in_hum++;
+				avg_internal_hums = avg_internal_hums + (data.getValue() - avg_internal_hums) / n_in_hum;
+			}
+			
+			break;
+			
+		//! Externo
+		case 302:
+			if (data.getUnit() == TEMPERATURE) {
+				n_out_temp++;
+				avg_external_temps = avg_external_temps + (data.getValue() - avg_external_temps) / n_out_temp;
+			} else { //! HUMIDITY
+				n_out_hum++;
+				avg_external_hums = avg_external_hums + (data.getValue() - avg_external_hums) / n_out_hum;
+			}
+			
+			break;
+			
+		//! Erro
+		default:
+			throw new Exception("Deu ruim!");
+		}
+		
+		//! Update learning model
+		if(_instances.size() >= 1000) {
+			update_model();
+		}
+	}
+	
+	public synchronized void update_model() {
+		
 	}
 	
 	public synchronized void updateControl(SmartData data) {
 		
-	}
 	}
 }
